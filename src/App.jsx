b56833c5d7305b925ba987461b3eff2d931175e8
dindon04 <150649289+dindon04@users.noreply.github.com>
2025-06-29 20:33:35 +0400

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,10 @@
+import AppLayout from './components/layout/AppLayout'
+import { CryptoContextProvider } from './context/crypto-context'
+
 export default function App() {
-  return <h1>React Crypto App</h1>
+  return (
+    <CryptoContextProvider>
+      <AppLayout />
+    </CryptoContextProvider>
+  )
 }
